204fa87cdc5cfc6709bcfe5ecb35017582c93d22
JaPheets <dat.nguyentien0102@example.com>
2023-05-26 13:59:43 +0700

    first comit

---
+++ src/main/java/com/poly/Asmjava5Application.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asmjava5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asmjava5Application.class, args);
+	}
+
+}
