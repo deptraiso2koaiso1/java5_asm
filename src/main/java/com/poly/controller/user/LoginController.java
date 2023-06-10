aeeb8ce02bfbb1d9fce82aeac05f6c66af907b33
deptraiso2koaiso1 <hodanhnhan1166@gmail.com>
2023-06-10 19:02:38 +0700

    cart-sort

--- src/main/java/com/poly/controller/user/LoginController.java
+++ src/main/java/com/poly/controller/user/LoginController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.poly.entities.Users;
-import com.poly.repositories.UsersDAO;
 import com.poly.service.CookieService;
 import com.poly.service.ParamService;
 import com.poly.service.SessionService;
@@ -28,38 +26,38 @@ public class LoginController {
 	ParamService paramService;
 	@Autowired
 	SessionService sessionService;
-	@Autowired
-	UsersDAO dao;
+//	@Autowired
+//	UsersDAO dao;
 	@Autowired
 	HttpServletRequest request;
 
 	@GetMapping("/login")
 	public String index(Model model) {
-		String un = cookieService.getValue("username");
-		String pw = cookieService.getValue("password");
-		model.addAttribute("username", un);
-		model.addAttribute("password", pw);
+//		String un = cookieService.getValue("username");
+//		String pw = cookieService.getValue("password");
+//		model.addAttribute("username", un);
+//		model.addAttribute("password", pw);
 		return "/user/login";
 	}
 
 	@PostMapping("/login")
 	public String checkLogin(Model model, @RequestParam("username") String username,
 			@RequestParam("password") String password) throws IOException {
-		System.out.println(username + password);
-		Users user = dao.findByUsername(username);
-		try {
-			if (user.getPassword().equals(password)) {
-				if (user.getIsAdmin() == true) {
-					request.setAttribute("message", "Login successful!");
-					response.sendRedirect("/admin");
-				} else {
-					response.sendRedirect("/shop");
-				}
-			}
-		} catch (Exception e) {
-			request.setAttribute("message", "Unexpected error, Please try again later!");
-		}
+//		System.out.println(username + password);
+//		Users user = dao.findByUsername(username);
+//		try {
+//			if (user.getPassword().equals(password)) {
+//				if (user.getIsAdmin() == true) {
+//					request.setAttribute("message", "Login successful!");
+//					response.sendRedirect("/admin");
+//				} else {
+//					response.sendRedirect("/shop");
+//				}
+//			}
+//		} catch (Exception e) {
+//			request.setAttribute("message", "Unexpected error, Please try again later!");
+//		}
 
-		return "/account/login";
+		return "/user/login";
 	}
 }
--- src/main/java/com/poly/repositories/UsersDAO.java
+++ src/main/java/com/poly/repositories/UsersDAO.java
@@ -4,11 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
-import com.poly.entities.Products;
 import com.poly.entities.Users;
 
 @EnableJpaRepositories
 @Repository
-public interface UsersDAO extends JpaRepository<Products, Integer> {
+public interface UsersDAO extends JpaRepository<Users, Integer> {
 	Users findByUsername(String username);
 }
